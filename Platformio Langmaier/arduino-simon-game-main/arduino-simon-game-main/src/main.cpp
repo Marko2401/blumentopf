b417c73283a8ff79ac0b0ceaff7be05790e6756d
MoritzLangmaier <moritz.langmaier@bulme.at>
2025-11-20 17:37:26 +0100

    Little counter Program

--- Platformio Langmaier/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
+++ Platformio Langmaier/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
@@ -1,6 +1,6 @@
-
 #include <Arduino.h>
 
+int counter = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -8,6 +8,28 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("ABC");
+  counter ++;
+
+  if (counter % 3 == 0)
+  {
+    if(counter % 5 == 0)
+    {
+      Serial.println("Fizz Buzz");
+    }
+  
+  else
+  {
+    Serial.println("Fizz");
+  }
+}
+  else{
+    if (counter % 5 == 0){
+      Serial.println("Buzz");
+    }
+    else{
+      Serial.println(counter);
+    }
+  }
+
   delay(500);
 }
